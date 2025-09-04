0551dce8badda3f439bd9081ff760943d351fe25
Felipe da Silva <felipedasilva1825@gmail.com>
2025-09-04 16:59:59 -0300

    criação e finalização da calculadora de soma em angular 04/09/2025

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HelloWorldComponent } from './helloworld/helloworld.component'
+import {CalculadoraComponent} from './calculadora/calculadora.component'
 
 @Component({
   selector: 'app-root',
-  imports: [HelloWorldComponent],
+  imports: [CalculadoraComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
