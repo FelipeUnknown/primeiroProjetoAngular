a02b239d17a979d1eac3dc963aadaac1c368e2a9
Felipe da Silva <felipedasilva1825@gmail.com>
2025-09-05 22:20:50 -0300

    05/09/25

--- src/app/lista-compras/lista-compras.component.ts
+++ src/app/lista-compras/lista-compras.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ItemLista } from './itemlista'
 
 @Component({
   selector: 'app-lista-compras',
@@ -10,10 +11,19 @@ import { FormsModule } from '@angular/forms';
 export class ListaComprasComponent {
 
   item:string='';
+  lista: ItemLista[] = [];
 
   adicionarItem(){
+    let itenLista = new ItemLista();
+    itenLista.nome = this.item;
+    itenLista.id = this.lista.length + 1;
+
+    this.lista.push(itenLista);
+
     console.log("Item recebido: ", this.item)
     this.item = '';
+
+    console.table(this.lista)
   };
 
 
